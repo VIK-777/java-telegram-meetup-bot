84498f60e9133a5b4d542cc9d7acdd18baa9686f
VIK-777 <36794502+VIK-777@users.noreply.github.com>
2024-05-01 16:26:42 +0200

    Add publishing build scans

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,13 @@
+plugins {
+    id("com.gradle.develocity") version ("3.17.2")
+}
+
+develocity {
+    buildScan {
+        publishing.onlyIf { System.getenv("CI") != null }
+        termsOfUseUrl = "https://gradle.com/help/legal-terms-of-use"
+        termsOfUseAgree = "yes"
+    }
+}
+
 rootProject.name = "meetupbot"
